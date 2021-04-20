e211a433ef0565058e1e08c29227ce29d1c0fbdb
Lenard Frommelt <lenny09918050@googlemail.com>
2021-04-20 10:07:07 +0200

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -2,7 +2,8 @@ module.exports = {
     getDependencies: () => {
         return {
             "mustache": "^4.2.0",
-            "secret-manager-client": "0.0.2"
+            "secret-manager-client": "0.0.2",
+            "vanillaqr": "^1.0.0"
         }
     }
 }
